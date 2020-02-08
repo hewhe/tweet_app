3bc8364945e0981d6895016618bc27325bda51e3
tama <yuki023201025@gmail.com>
2020-02-08 14:54:36 +0900

    post関係のview,controllerの作成終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
   def index
-  	@posts = Post.all.order(created_at: :desc)
+  	@posts = Post.all
   end
 
   def show
   	@post = Post.find(params[:id])
-  end
-
-  def edit
   end
 
   def new
@@ -16,13 +13,38 @@ class PostsController < ApplicationController
 
   def create
   	@post = Post.new(post_nparams)
-  	@post.save
-  	redirect_to("/posts")
+  	if @post.save
+  		flash[:notice] = "成功！！"
+  		redirect_to("/posts")
+  	else
+  		render("posts/new")
+  	end
 
   	# form_for用のcreate
   	# @post = Post.new(post_params)
   	# @post.save
   	# redirect_to("/post/index")
+  end
+
+  def edit
+	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find_by(id: params[:id])
+  	if @post.update(post_nparams)
+  		flash[:notice] = "成功！"
+  		redirect_to("/posts/#{@post.id}")
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+	flash[:notice] = "削除！！"
+	redirect_to posts_path
   end
 
   private
